fe68e41a29443602e5d7153128d36918b91d54b2
rex ardiente <rexardiente09@gmail.com>
2020-11-10 16:49:11 +0800

    Update PostgresDriver.scala

--- app/utils/db/PostgresDriver.scala
+++ app/utils/db/PostgresDriver.scala
@@ -2,8 +2,32 @@ package utils.db
 
 import com.github.tminglei.slickpg._
 
-trait PostgresDriver extends ExPostgresProfile with PgDate2Support {
-  override val api = new API with DateTimeImplicits with Date2DateTimePlainImplicits
+trait PostgresDriver extends ExPostgresProfile with PgArraySupport
+  with PgDate2Support
+  with PgRangeSupport
+  with PgHStoreSupport
+  with PgSearchSupport
+  with PgNetSupport
+  with PgLTreeSupport {
+  
+  override val api = MyAPI
+
+  object MyAPI extends API with ArrayImplicits
+    with DateTimeImplicits
+    with NetImplicits
+    with LTreeImplicits
+    with RangeImplicits
+    with HStoreImplicits
+    with SearchImplicits
+    with SearchAssistants {
+    implicit val intListTypeMapper = new SimpleArrayJdbcType[Int]("integer").to(_.toList)
+    implicit val intListListTypeMapper = new AdvancedArrayJdbcType[List[Int]]("integer[]",
+      utils.SimpleArrayUtils.fromString[List[Int]](s => 
+        scala.util.Try(s.substring(5, s.length - 1).split(",").map(_.trim.toInt).toList).getOrElse(List())
+      )(_).orNull,
+      utils.SimpleArrayUtils.mkString[List[Int]](_.toString)
+    ).to(_.toList)
+  }
 }
 
 object PostgresDriver extends PostgresDriver
