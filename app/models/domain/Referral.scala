a993dd323f015bfe90089d4a0f197ea5318ec8c6
Pacifico Mandolado Jr <mandoladopacificojr@gmail.com>
2020-11-25 15:44:08 +0800

    updated referral api

--- app/models/domain/Referral.scala
+++ app/models/domain/Referral.scala
@@ -5,7 +5,7 @@ import java.time.Instant
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-case class Referral(id: UUID, name : String, gameID: UUID,  imgURl: String, amount: Double, referralreated: Long)
+case class Referral(id: UUID, name : String, gameID: UUID,  imgURl: String, amount: Double, referralcreated: Long)
 
 object c {
 	implicit def implReferral = Json.format[Referral]
