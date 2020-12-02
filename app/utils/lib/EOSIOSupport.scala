130db15823f4fc5a8f75730572dc4a54f113457a
rex ardiente <rexardiente09@gmail.com>
2020-12-02 10:59:57 +0800

    Update: Test Codes

--- app/utils/lib/EOSIOSupport.scala
+++ app/utils/lib/EOSIOSupport.scala
@@ -135,40 +135,6 @@ class EOSIOSupport @Inject()(ws: WSClient)(implicit ec: scala.concurrent.Executi
       .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
   }
 
-  def hi(user: String): Unit = {
-    try {
-      // unlockWalletAPI()
-      abiJsonToBin("hello", "hi", Seq(user)).map { data => 
-        val actions: List[TransactionAction] = Arrays.asList(new TransactionAction(
-            "hello",
-            "hi",
-            authorization(Seq(user)), 
-            data.map(_.binargs.as[String]).getOrElse(null)))
-
-        val packedTx: PackedTransaction = new PackedTransaction()
-            packedTx.setExpiration(LocalDateTime.parse(expirationInStr))
-            packedTx.setRefBlockNum(getLatestBlock.getBlockNum())
-            packedTx.setRefBlockPrefix(getLatestBlock.getRefBlockPrefix())
-            packedTx.setDelaySec(0)
-            packedTx.setMaxNetUsageWords(0)
-            packedTx.setMaxCpuUsageMs(0)
-            packedTx.setActions(actions)
-
-        val signedPackedTransaction: SignedPackedTransaction = 
-            signTransaction(
-              packedTx,
-              Seq(publicKey),
-              clientNodeosAPI.getChainInfo().getChainId())
-
-        val pushedTransaction: PushedTransaction = clientNodeosAPI.pushTransaction(null, signedPackedTransaction)
-
-        println(mapper.writeValueAsString(pushedTransaction))
-      }
-    } catch  {
-      case e: Throwable => println(e)
-    }
-  }
-
   // sign trasanction in JSON response
   private def signTransactionToJson(args: Option[BinaryArgs]): Future[JsValue] = {
     val request: WSRequest = ws.url(keosdApiBaseURL + config.getString("eosio.uri.path.sign_transaction"))
