6226c2df48fa230eaf5755df9ac039b38f34fee6
Pacifico Mandolado Jr <mandoladopacificojr@gmail.com>
2020-12-17 19:49:03 +0800

    authenticaiton api

--- app/models/dao/LoginDAO.scala
+++ app/models/dao/LoginDAO.scala
@@ -1,22 +1,24 @@
 package models.dao
 
+import javax.inject.{ Inject, Singleton }
 import java.util.UUID
 import java.time.Instant
-import javax.inject.{ Inject, Singleton }
+import play.api.libs.json._
 import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
 import models.domain.Login
 
 @Singleton
 final class LoginDAO @Inject()(
-    protected val dbConfigProvider: DatabaseConfigProvider
-  ) extends HasDatabaseConfigProvider[utils.db.PostgresDriver] {
+    protected val dbConfigProvider: DatabaseConfigProvider,
+  ) extends HasDatabaseConfigProvider[utils.db.PostgresDriver] with utils.ColumnTypeImplicits {
   import profile.api._
+
 
   protected class LoginTable(tag: Tag) extends Table[Login](tag, "LOGIN") {
     def id = column[UUID] ("ID")
     def username = column[String] ("USERNAME")
     def password = column[String] ("PASSWORD")
-    def logincreated = column[Instant] ("LOGINCREATED")
+    def logincreated = column[Long] ("LOGINCREATED")
 
    def * = (id, username, password, logincreated) <> ((Login.apply _).tupled, Login.unapply)
   }
--- app/models/repo/LoginRepo.scala
+++ app/models/repo/LoginRepo.scala
@@ -20,9 +20,43 @@ class LoginRepo @Inject()(
     db.run(dao.Query += login)
 
   def delete(id: UUID): Future[Int] =
-    db.run(dao.Query(id).delete)
-
+    db.run(dao.Query(id).delete)  
+//new
   def update(login: Login): Future[Int] =
     db.run(dao.Query.filter(_.id ===login.id).update(login))
 
-  }
+  def all(limit: Int, offset: Int): Future[Seq[Login]] =
+    db.run(dao.Query
+      .drop(offset)
+      .take(limit)
+      .result)
+
+ def exist(id: UUID): Future[Boolean] = 
+    db.run(dao.Query(id).exists.result)
+
+  def findByID(id: UUID, limit: Int, offset: Int): Future[Seq[Login]] =
+    db.run(dao.Query.filter(r => r.id === id  )
+      .drop(offset)
+      .take(limit)
+      .result)
+      
+  def findByDaily( currentdate: Long, limit: Int, offset: Int): Future[Seq[Login]] = 
+   db.run(dao.Query.filter(r =>  r.logincreated === currentdate) 
+     .drop(offset)
+     .take(limit)
+     .result)
+      
+ 
+  def findByDateRange(startdate: Long, enddate : Long, limit: Int, offset: Int): Future[Seq[Login]] =
+   db.run(dao.Query.filter(r => r.logincreated >= startdate && r.logincreated <= enddate ) 
+     .drop(offset)
+     .take(limit)
+     .result)
+
+  def findAll(limit: Int, offset: Int): Future[Seq[Login]] = 
+    db.run(dao.Query.drop(offset).take(limit).result)
+
+  def getSize(): Future[Int] =  
+    db.run(dao.Query.length.result)      
+
+ }
