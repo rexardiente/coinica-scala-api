a56a49e5eaab38d67886a820f97edc91f20be431
Pacifico Mandolado Jr <mandoladopacificojr@gmail.com>
2020-12-18 18:28:41 +0800

    loginstructure

--- app/models/dao/LoginDAO.scala
+++ app/models/dao/LoginDAO.scala
@@ -15,15 +15,15 @@ final class LoginDAO @Inject()(
 
 
   protected class LoginTable(tag: Tag) extends Table[Login](tag, "LOGIN") {
-    def id = column[UUID] ("ID")
+   
     def username = column[String] ("USERNAME")
     def password = column[String] ("PASSWORD")
-    def logincreated = column[Long] ("LOGINCREATED")
+    
 
-   def * = (id, username, password, logincreated) <> ((Login.apply _).tupled, Login.unapply)
+   def * = ( username, password) <> ((Login.apply _).tupled, Login.unapply)
   }
 
   object Query extends TableQuery(new LoginTable(_)) {
-    def apply(id: UUID) = this.withFilter(_.id === id)
+    def apply(username: String) = this.withFilter(_.username === username)
   }
 }
