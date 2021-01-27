71d245e7dbbc1186d875c40c37c9034ce1b114f1
rex ardiente <rexardiente09@gmail.com>
2021-01-28 01:42:09 +0800

    Update: Restructure Code Base

--- app/utils/ColumnTypeImplicits.scala
+++ app/utils/ColumnTypeImplicits.scala
@@ -7,7 +7,7 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
 import models.domain.eosio.{ Act, ActionTrace, Data, Partial, Receipt, Trace, GQGameStatus }
-import models.domain.Roles
+import models.domain.enum._
 import ejisan.kuro.otp.OTPKey
 import ejisan.scalauthx.HashedCredential
 
@@ -20,38 +20,55 @@ trait ColumnTypeImplicits extends HasDatabaseConfigProvider[utils.db.PostgresDri
      s => Json.toJson(s),
      i => i.as[Trace])
   implicit val stringListMapper = MappedColumnType.base[Seq[ActionTrace], JsValue](
-    list => JsArray(list.map(Json.toJson(_))),
-    js => js.as[Seq[ActionTrace]])
+    s => JsArray(s.map(Json.toJson(_))),
+    i => i.as[Seq[ActionTrace]])
   implicit val partialColumnMapper = MappedColumnType.base[Partial, JsValue](
-     s => Json.toJson(s),
-     i => i.as[Partial])
+    s => Json.toJson(s),
+    i => i.as[Partial])
   implicit val dataColumnMapper = MappedColumnType.base[Data, JsValue](
-     s => Json.toJson(s),
-     i => i.as[Data])
+    s => Json.toJson(s),
+    i => i.as[Data])
   implicit val receiptColumnMapper = MappedColumnType.base[Receipt, JsValue](
-     s => Json.toJson(s),
-     i => i.as[Receipt])
+    s => Json.toJson(s),
+    i => i.as[Receipt])
   implicit val actColumnMapper = MappedColumnType.base[Act, JsValue](
-     s => Json.toJson(s),
-     i => i.as[Act])
+    s => Json.toJson(s),
+    i => i.as[Act])
   implicit val gqListGameStatusColumnMapper = MappedColumnType.base[List[GQGameStatus], JsValue](
-     s => Json.toJson(s),
-     i => i.as[List[GQGameStatus]])
+    s => Json.toJson(s),
+    i => i.as[List[GQGameStatus]])
   implicit val jodaTimeMapping: BaseColumnType[DateTime] = MappedColumnType.base[DateTime, Timestamp](
     dateTime => new Timestamp(dateTime.getMillis),
-    timeStamp => new DateTime(timeStamp.getTime)
-  )
-  implicit val instantColumnType: BaseColumnType[Instant] =
-    MappedColumnType.base[Instant, Timestamp](
-        instant => Timestamp.from(instant),
-        ts => ts.toInstant
-    )
-  // Ejisan Custom Mappers..
+    timeStamp => new DateTime(timeStamp.getTime))
+  implicit val instantColumnType: BaseColumnType[Instant] = MappedColumnType.base[Instant, Timestamp](
+    s => Timestamp.from(s),
+    i => i.toInstant)
+
+  // Custom Value Mappers..
   implicit val roleMapper = MappedColumnType.base[Roles.Role, String](
-  e => e.toString,
-  s => Roles.withName(s))
+    e => e.toString,
+    s => Roles.withName(s))
   implicit val optKeyMapper = MappedColumnType.base[OTPKey, String](
-  _.toBase32, OTPKey.fromBase32(_))
+    _.toBase32, OTPKey.fromBase32(_))
   implicit val hashedCredentialMapper = MappedColumnType.base[HashedCredential, String](
-  _.toString, HashedCredential.fromString(_))
+    _.toString, HashedCredential.fromString(_))
+
+  // Custom VIP Value Mappers..
+  implicit val vipMapper = MappedColumnType.base[VIP.value, String](
+    e => e.toString,
+    s => VIP.withName(s))
+  implicit val vipBenefitsAmountMapper = MappedColumnType.base[VIPBenefitsAmount.value, Long](
+    e => (try {
+          e.toString.toLong
+        } catch {
+          case _: Throwable => 0L
+        }),
+    s => VIPBenefitsAmount.withName(s.toString))
+  implicit val vipBenefitsPointsMapper = MappedColumnType.base[VIPBenefitsPoints.value, Long](
+    e => (try {
+          e.toString.toLong
+        } catch {
+          case _: Throwable => 0L
+        }),
+    s => VIPBenefitsPoints.withName(s.toString))
 }
