e28996e10ca440b5c65807d2601aa6c96d872ca2
rex ardiente <rexardiente09@gmail.com>
2021-02-16 13:48:51 +0800

    Update GQBattleCalculation.scala

--- app/utils/lib/GQBattleCalculation.scala
+++ app/utils/lib/GQBattleCalculation.scala
@@ -9,7 +9,7 @@ import models.domain.eosio.{ GQCharacterData, GameLog, GQBattleResult }
 class GQBattleCalculation[T <: GQCharacterData](plyr1: T, plyr2: T) {
 	// Initialize game default values..
 	private val gameID 			 : UUID 									= java.util.UUID.randomUUID
-	private val characters 	 : Map[String, T] 				= Map(plyr1.id -> plyr1, plyr2.id -> plyr2)
+	private val characters 	 : Map[String, T] 				= if (plyr1.speed > plyr2.speed) Map(plyr1.id -> plyr1, plyr2.id -> plyr2) else Map(plyr2.id -> plyr2, plyr1.id -> plyr1)
 	private val logs  			 : ListBuffer[GameLog] 		= ListBuffer.empty[GameLog]
 	private var battleResult : Option[GQBattleResult] = None
 	private var gameRounds   : Int = 1
@@ -20,60 +20,85 @@ class GQBattleCalculation[T <: GQCharacterData](plyr1: T, plyr2: T) {
 		if (plyr1.character_life == 0 || plyr2.character_life == 0) None
 		else
 			while (plyr1TotalDmg < characters(plyr1.id).initial_hp && plyr2TotalDmg < characters(plyr2.id).initial_hp) {
-				// determine which monster attack first
-				if (plyr1.speed > plyr2.speed) {
-					// perform damage calculation
-					val partialDmg2: (GameLog, Int) = dmgOutput(characters(plyr1.id), characters(plyr2.id))
-					logs += partialDmg2._1
-					plyr2TotalDmg += partialDmg2._2
-					gameRounds += 1
+				// perform damage calculation
+				val partialDmg2: (GameLog, Int) = dmgOutput(characters.head._2, characters.last._2)
+				logs += partialDmg2._1
+				plyr2TotalDmg += partialDmg2._2
+				gameRounds += 1
 
-					if (characters(plyr2.id).initial_hp > plyr2TotalDmg) {
-						val partialDmg1: (GameLog, Int) = dmgOutput(characters(plyr2.id), characters(plyr1.id))
-						logs += partialDmg1._1
-						plyr1TotalDmg += partialDmg1._2
-						gameRounds += 1
-
-						if (characters(plyr1.id).initial_hp < plyr1TotalDmg)
-							battleResult = Some(GQBattleResult(
-																	gameID,
-																	Map(characters(plyr1.id).id -> true,
-																			characters(plyr2.id).id -> false),
-																	logs.toList))
-					}
-					else battleResult = Some(GQBattleResult(
-																	gameID,
-																	Map(characters(plyr2.id).id -> true,
-																			characters(plyr1.id).id -> false),
-																	logs.toList))
-	      }
-
-	      else
-				{
-					val partialDmg1: (GameLog, Int) = dmgOutput(characters(plyr2.id), characters(plyr1.id))
+				if (characters.last._2.initial_hp > plyr2TotalDmg) {
+					val partialDmg1: (GameLog, Int) = dmgOutput(characters.last._2, characters.head._2)
 					logs += partialDmg1._1
 					plyr1TotalDmg += partialDmg1._2
 					gameRounds += 1
 
-				  if (characters(plyr1.id).initial_hp > plyr1TotalDmg) {
-				  	val partialDmg2: (GameLog, Int) = dmgOutput(characters(plyr1.id), characters(plyr2.id))
-						logs += partialDmg2._1
-						plyr2TotalDmg += partialDmg2._2
-						gameRounds += 1
-
-						if (characters(plyr2.id).initial_hp < plyr2TotalDmg)
-							battleResult = Some(GQBattleResult(
-																	gameID,
-																	Map(characters(plyr2.id).id -> true,
-																			characters(plyr1.id).id -> false),
-																	logs.toList))
-					}
-				  else battleResult = Some(GQBattleResult(
-														  		gameID,
-																	Map(characters(plyr1.id).id -> true,
-																			characters(plyr2.id).id -> false),
-																	logs.toList))
+					if (characters.head._2.initial_hp < plyr1TotalDmg)
+						battleResult = Some(GQBattleResult(
+																gameID,
+																Map(characters.last._2.id -> true,
+																		characters.head._2.id -> false),
+																logs.toList))
 				}
+				else battleResult = Some(GQBattleResult(
+																gameID,
+																Map(characters.head._2.id -> true,
+																		characters.last._2.id -> false),
+																	logs.toList))
+
+				// determine which monster attack first
+				// if (plyr1.speed > plyr2.speed) {
+				// 	// perform damage calculation
+				// 	val partialDmg2: (GameLog, Int) = dmgOutput(characters(plyr1.id), characters(plyr2.id))
+				// 	logs += partialDmg2._1
+				// 	plyr2TotalDmg += partialDmg2._2
+				// 	gameRounds += 1
+
+				// 	if (characters(plyr2.id).initial_hp > plyr2TotalDmg) {
+				// 		val partialDmg1: (GameLog, Int) = dmgOutput(characters(plyr2.id), characters(plyr1.id))
+				// 		logs += partialDmg1._1
+				// 		plyr1TotalDmg += partialDmg1._2
+				// 		gameRounds += 1
+
+				// 		if (characters(plyr1.id).initial_hp < plyr1TotalDmg)
+				// 			battleResult = Some(GQBattleResult(
+				// 													gameID,
+				// 													Map(characters(plyr2.id).id -> true,
+				// 															characters(plyr1.id).id -> false),
+				// 													logs.toList))
+				// 	}
+				// 	else battleResult = Some(GQBattleResult(
+				// 													gameID,
+				// 													Map(characters(plyr1.id).id -> true,
+				// 															characters(plyr2.id).id -> false),
+				// 													logs.toList))
+	   //    }
+
+	   //    else
+				// {
+				// 	val partialDmg1: (GameLog, Int) = dmgOutput(characters(plyr2.id), characters(plyr1.id))
+				// 	logs += partialDmg1._1
+				// 	plyr1TotalDmg += partialDmg1._2
+				// 	gameRounds += 1
+
+				//   if (characters(plyr1.id).initial_hp > plyr1TotalDmg) {
+				//   	val partialDmg2: (GameLog, Int) = dmgOutput(characters(plyr1.id), characters(plyr2.id))
+				// 		logs += partialDmg2._1
+				// 		plyr2TotalDmg += partialDmg2._2
+				// 		gameRounds += 1
+
+				// 		if (characters(plyr2.id).initial_hp < plyr2TotalDmg)
+				// 			battleResult = Some(GQBattleResult(
+				// 													gameID,
+				// 													Map(characters(plyr1.id).id -> true,
+				// 															characters(plyr2.id).id -> false),
+				// 													logs.toList))
+				// 	}
+				//   else battleResult = Some(GQBattleResult(
+				// 										  		gameID,
+				// 													Map(characters(plyr2.id).id -> true,
+				// 															characters(plyr1.id).id -> false),
+				// 													logs.toList))
+				// }
 			}
 	}
 
