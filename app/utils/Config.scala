2e628030ff1ce7d02f621d2eba67f8f17c69abec
rex ardiente <rexardiente09@gmail.com>
2021-06-24 16:08:20 +0800

    Update Config.scala

--- app/utils/Config.scala
+++ app/utils/Config.scala
@@ -9,6 +9,7 @@ object Config {
 	val config = ConfigFactory.load()
 	val DEFAULT_SYSTEM_SCHEDULER_TIMER: Int = config.getInt("platform.default.system.scheduler")
 	val GQ_DEFAULT_BATTLE_TIMER: Int = config.getInt("platform.games.GQ.battle.timer")
+	val SUPPORTED_CURRENCIES: List[String] = config.getStringList("platform.supported.currencies").asScala.toList
 	// Ghost Quest
 	val GQ: List[String] = config.getStringList("platform.games.contracts.ghostquest").asScala.toList
 	val GQ_CODE: String = GQ(0)
