800de0c8c52a2f32444af04846779f52de369bc3
rex ardiente <rexardiente09@gmail.com>
2021-11-22 20:09:30 +0800

    Update: MJ API

--- app/utils/lib/MahjongHiloEOSIO.scala
+++ app/utils/lib/MahjongHiloEOSIO.scala
@@ -183,4 +183,17 @@ class MahjongHiloEOSIO @Inject()(implicit ws: WSClient, ec: ExecutionContext) {
         else None
       }.recover { case e: Exception => None }
   }
+  def riichiDiscard(id: Int): Future[Option[String]] =  {
+    val request: WSRequest = ws.url(nodeServerURI +  "/mahjong-hilo/riichi-discard")
+    val complexRequest: WSRequest = request
+      .addHttpHeaders("Accept" -> "application/json")
+      .withRequestTimeout(10000.millis)
+
+    complexRequest.post(Json.obj("id" -> id))
+      .map { v =>
+        if (!(v.json \ "error").asOpt[Boolean].getOrElse(true) && (v.json \ "code").asOpt[Int].getOrElse(0) == 200)
+          (v.json \ "data" \ "transaction_id").asOpt[String]
+        else None
+      }.recover { case e: Exception => None }
+  }
 }
